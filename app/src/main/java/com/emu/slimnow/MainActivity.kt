b7601546775e4b2d0580aa143b18dafc8704a5c8
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2023-05-11 13:13:27 -0300

    Added Splash Screen with a "workload placeholder"

--- app/src/main/java/com/emu/slimnow/MainActivity.kt
+++ app/src/main/java/com/emu/slimnow/MainActivity.kt
@@ -3,10 +3,13 @@ package com.emu.slimnow
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.fragment.app.Fragment
 import com.emu.slimnow.databinding.ActivityMainBinding
+import com.emu.slimnow.model.MainActivityModel
 import com.emu.slimnow.prefer.SettingsActivity
 import com.emu.slimnow.ui.*
 
@@ -41,8 +44,17 @@ class MainActivity : AppCompatActivity() {
         activityIntent.apply { startActivity(activityIntent) }
     }
 
+    private val mainViewModel: MainActivityModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                mainViewModel.isLoading.value
+            }
+            //setOnExitAnimationListener {}
+        }
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
---
+++ app/src/main/java/com/emu/slimnow/model/MainActivityModel.kt
@@ -0,0 +1,26 @@
+package com.emu.slimnow.model
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class MainActivityModel : ViewModel() {
+    private val _loadingApp = MutableStateFlow(true)
+    val isLoading = _loadingApp.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            // We can load all internal components before the main activity being rendering here
+            // Like starts the emulator, load all library entries points, start some profile tools
+            // like Perfetto and others
+            delay(2_000)
+            // We're confirming that we can go to main screen now!
+            _loadingApp.value = false
+        }
+    }
+
+
+}
