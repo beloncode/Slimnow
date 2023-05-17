5490017a41c894865678549cfb44115eefe386a1
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2023-05-17 08:27:58 -0300

    Handling attachToJavaVM on a specific function, it is more likely for generic thread/pool

--- app/src/main/cpp/JNInterface.cpp
+++ app/src/main/cpp/JNInterface.cpp
@@ -1,15 +1,50 @@
 // This source code is part of SlimNow project
+#include <thread>
 #include <jni.h>
 
 #include "SlimVM.h"
 
-namespace Slim {
+namespace Slim::Thread {
+    thread_local pthread_key_t t_pthreadKey;
+    thread_local auto t_hasAttached{false};
 
-    void loadVirtualSystem() {
-        g_psxMachine = std::make_unique<VirtualMachine>();
+    void onNativeExit(void* javaVM) {
+        auto vm{reinterpret_cast<JavaVM*>(javaVM)};
+        if (t_hasAttached) {
+            vm->DetachCurrentThread();
+        }
 
+        // If we reach here, we already have a valid pthread key!
+        pthread_key_delete(t_pthreadKey);
     }
 
+    auto attachToJavaVM(JavaVM* vm, const std::string& thName) {
+        JNIEnv* local{};
+        pthread_setname_np(pthread_self(), thName.c_str());
+
+        if (vm->GetEnv(reinterpret_cast<void**>(&local),
+                       JNI_VERSION_1_6) == JNI_EDETACHED) {
+            vm->AttachCurrentThread(&local, nullptr);
+            t_hasAttached = true;
+        }
+
+        pthread_key_create(&t_pthreadKey, onNativeExit);
+        pthread_setspecific(t_pthreadKey, vm);
+
+        return local;
+    }
+}
+
+namespace Slim::Unordered {
+    [[maybe_unused]] auto psxVMExist{false};
+
+    void createPSXVM(JavaVM* vm) {
+        Thread::attachToJavaVM(vm, "Spawn PSX-VM");
+
+        g_psxMachine = std::make_unique<VirtualMachine>();
+        psxVMExist = g_psxMachine != nullptr;
+
+    }
 }
 
 // Called from JNI native libraries loader, after load our library!
@@ -19,7 +54,8 @@ JNI_OnLoad([[maybe_unused]] JavaVM* vm, [[maybe_unused]] void* reserved) {
     const auto jniVersion{JNI_VERSION_1_6};
 
     // Creating our emulator's virtual machine instance while the app is loading!
-    Slim::loadVirtualSystem();
+    std::thread startThread(Slim::Unordered::createPSXVM, vm);
+    startThread.join();
 
     return jniVersion;
 }
